3f2c45632842e1097b0cc87ecd2177838b04107d
Mr.Jor <VapidBoys@163.com>
2023-01-19 17:14:10 +0800

    配置了自动保存折叠信息

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -4,3 +4,9 @@ local G = require('G')
 G.cmd([[ au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif ]])
 
 G.cmd([[ au BufEnter * if &buftype == '' && &readonly == 1 | set buftype=acwrite | set noreadonly | endif ]])
+
+-- 自动保存折叠信息
+G.cmd([[ 
+  au FileType * try | silent! mkview | catch | endtry
+  au BufLeave,BufWinEnter * silent! loadview
+]])
