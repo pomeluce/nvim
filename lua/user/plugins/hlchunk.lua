72e503cfd1801f99a9a61e5979daaebe046f32dc
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2023-12-03 14:57:08 +0800

    adjusted hlchunk config

--- lua/user/plugins/hlchunk.lua
+++ lua/user/plugins/hlchunk.lua
@@ -8,30 +8,25 @@ function M.setup()
   return {
     chunk = {
       style = {
-        { fg = '#61A3BA' },
+        { fg = '#E47E67' },
         { fg = '#c21f30' }, -- 这个高亮是用来标志错误的代码块
       },
+      use_treesitter = false,
     },
     indent = {
-      chars = { '│' }, -- 更多的字符可以在 https://unicodeplus.com/ 这个网站上找到
-      style = {
-        '#E06C75',
-        '#E5C07B',
-        '#61AFEF',
-        '#D19A66',
-        '#98C379',
-        '#C678DD',
-        '#56B6C2',
-      },
+      -- 更多的字符可以在 https://unicodeplus.com/ 这个网站上找到
+      chars = { '│' },
+      style = { '#4C5257' },
     },
     line_num = {
       enable = true,
       use_treesitter = false,
-      style = '#61A3BA',
+      style = '#E47E67',
     },
     blank = {
       enable = false,
     },
   }
 end
+
 return M
