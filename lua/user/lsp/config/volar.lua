88b4771681be398929e5c744db37c4b56ebe7ba0
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2024-03-29 00:47:27 +0800

    修复了 volar 插件不生效的问题

--- lua/user/lsp/config/volar.lua
+++ lua/user/lsp/config/volar.lua
@@ -1,21 +1,3 @@
-local function get_ts_server_path(root_dir)
-  local util = require('lspconfig.util')
-
-  local global_ts = '/usr/lib/node_modules/typescript/lib/tsserverlibrary.js'
-  local found_ts = ''
-  local function check_dir(path)
-    found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
-    if util.path.exists(found_ts) then
-      return path
-    end
-  end
-  if util.search_ancestors(root_dir, check_dir) then
-    return found_ts
-  else
-    return global_ts
-  end
-end
-
 return {
   cmd = { 'vue-language-server', '--stdio' },
   -- 启用 volar 接管模式
@@ -25,8 +7,8 @@ return {
     typescript = {
       tsdk = '/usr/lib/node_modules/typescript/lib/',
     },
+    vue = {
+      hybridMode = false,
+    },
   },
-  on_new_config = function(new_config, new_root_dir)
-    new_config.init_options.typescript.serverPath = get_ts_server_path(new_root_dir)
-  end,
 }
