aba64d737bb425c9c390bd386cd79f6dac6eeebc
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2024-04-22 09:32:11 +0800

    fix: 修复了 wsl 中粘贴中文乱码问题

--- lua/user/core/profile.lua
+++ lua/user/core/profile.lua
@@ -142,3 +142,19 @@ vim.cmd([[
     let &t_SI.="\e[5 q"
     let &t_EI.="\e[1 q"
 ]])
+
+-- wsl 剪切板
+if is_wsl then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw -Format UnicodeText).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw -Format UnicodeText).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
