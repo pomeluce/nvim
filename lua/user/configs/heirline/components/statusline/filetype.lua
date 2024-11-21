325bad6a99c8ad2b9aa1585863e4c193e59eabba
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2024-11-21 23:33:31 +0800

    fix: 修复了状态栏文件类型显示问题

--- lua/user/configs/heirline/components/statusline/filetype.lua
+++ lua/user/configs/heirline/components/statusline/filetype.lua
@@ -2,7 +2,7 @@ local colors = require('user.configs.heirline.colors')
 
 return {
   provider = function()
-    return ' 󰈔 %2(' .. string.lower(vim.bo.filetype) .. '%) '
+    return vim.bo.filetype ~= '' and ' 󰈔 %2(' .. string.lower(vim.bo.filetype) .. '%) '
   end,
   hl = { fg = colors.cyan },
 }
