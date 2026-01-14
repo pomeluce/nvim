3e4af786318c7b97d2e94f63f29a922b2e917adc
Tso <62343478+pomeluce@users.noreply.github.com>
2026-01-14 16:07:46 +0800

    fix: treesitter ft issue

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -76,16 +76,12 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function(event)
     local ok, nvim_treesitter = pcall(require, 'nvim-treesitter')
     if not ok then return end
-
     local parsers = require('nvim-treesitter.parsers')
-    if not parsers[event.match] or not nvim_treesitter.install then return end
-
-    local ft = vim.bo[event.buf].ft
-    local lang = vim.treesitter.language.get_lang(ft)
-
+    local lang = vim.treesitter.language.get_lang(event.match)
+    if not parsers[lang] or not nvim_treesitter.install then return end
     nvim_treesitter.install({ lang }):await(function(err)
       if err then
-        vim.notify('Treesitter install error for ft: ' .. ft .. ' err: ' .. err)
+        vim.notify('Treesitter install error for ft: ' .. event.match .. ' err: ' .. err)
         return
       end
       pcall(vim.treesitter.start, event.buf)
