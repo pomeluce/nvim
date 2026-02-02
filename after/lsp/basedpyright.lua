c4154d02a9d9e4da51c45a196dd58f751ecdf4c0
Tso <62343478+pomeluce@users.noreply.github.com>
2026-02-02 15:50:10 +0800

    feat: basedpyright lsp config

--- after/lsp/basedpyright.lua
+++ after/lsp/basedpyright.lua
@@ -3,18 +3,21 @@ return {
   settings = {
     basedpyright = {
       analysis = {
-        ignorePatterns = { '*.pyi' },
+        typeCheckingMode = 'standard',
+        autoSearchPaths = true, -- 自动将项目子路径添加到搜索路径
+        useLibraryCodeForTypes = true, -- 对第三方库进行类型推断
+        disableOrganizeImports = true, -- 禁用自动导入排序
+        ignorePatterns = { '*.pyi', '**/__pycache__/**', '**/node_modules/**' }, -- 文件忽略模式
+
+        diagnosticMode = 'openFilesOnly', -- 只对打开的文件报错
         diagnosticSeverityOverrides = {
-          reportCallIssue = 'warning',
-          reportUnreachable = 'warning',
-          reportUnusedImport = 'none',
-          reportUnusedCoroutine = 'warning',
+          reportMissingTypeStubs = 'none', -- 忽略没有类型提示的第三方库的警告
+          reportUnreachable = 'warning', -- 对永远执行不到的代码进行警告
+          reportUnusedImport = 'warning', -- 对没有使用的 import 警告
+          reportUnusedCoroutine = 'warning', -- 对 async 函数调用时未添加 await 进行警告
+          reportUnknownMemberType = 'none', -- 忽略调用对象属性时未知类型警告
+          -- reportCallIssue = 'none', -- 函数调用问题检查
         },
-        -- diagnosticMode = "workspace",
-        diagnosticMode = 'openFilesOnly',
-        typeCheckingMode = 'basic',
-        reportCallIssue = 'none',
-        disableOrganizeImports = true,
       },
     },
   },
