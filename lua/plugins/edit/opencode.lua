94c01292d08388bbcaef4d6bfc7e0fcadac4b9e2
Tso <62343478+pomeluce@users.noreply.github.com>
2026-03-31 15:52:41 +0800

    chore: 更改 opencode 切换快捷键为 <leader>oc

--- lua/plugins/edit/opencode.lua
+++ lua/plugins/edit/opencode.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
     map({ 'n', 'x' }, '<leader>oa', function() require('opencode').ask() end, { desc = 'Ask opencode…' })
     map({ 'n', 'x' }, '<leader>ol', function() require('opencode').select() end, { desc = 'Execute opencode action…' })
-    map({ 'n', 't' }, '<leader>ot', function() require('opencode').toggle() end, { desc = 'Toggle opencode' })
+    map({ 'n', 't' }, '<leader>oc', function() require('opencode').toggle() end, { desc = 'Toggle opencode' })
     map({ 'n', 'x' }, '<leader>os', function() return require('opencode').operator('@this ') end, { desc = 'Add range to opencode', expr = true })
     map('n', '<leader>os', function() return require('opencode').operator('@this ') .. '_' end, { desc = 'Add line to opencode', expr = true })
   end,
