55aed2d948a5c315ea01f4604e82bf343136e42c
Tso <62343478+pomeluce@users.noreply.github.com>
2026-03-31 15:41:33 +0800

    feat: 添加 opencode.nvim 插件支持

---
+++ lua/plugins/edit/opencode.lua
@@ -0,0 +1,16 @@
+vim.pack.add({
+  { src = 'https://github.com/nickjvandyke/opencode.nvim' },
+})
+
+local map = vim.keymap.set
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = vim.api.nvim_create_augroup('SetupOpencode', { clear = true }),
+  callback = function()
+    map({ 'n', 'x' }, '<leader>oa', function() require('opencode').ask() end, { desc = 'Ask opencode…' })
+    map({ 'n', 'x' }, '<leader>ol', function() require('opencode').select() end, { desc = 'Execute opencode action…' })
+    map({ 'n', 't' }, '<leader>ot', function() require('opencode').toggle() end, { desc = 'Toggle opencode' })
+    map({ 'n', 'x' }, '<leader>os', function() return require('opencode').operator('@this ') end, { desc = 'Add range to opencode', expr = true })
+    map('n', '<leader>os', function() return require('opencode').operator('@this ') .. '_' end, { desc = 'Add line to opencode', expr = true })
+  end,
+})
